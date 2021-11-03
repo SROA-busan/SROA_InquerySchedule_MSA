cdb909721fe94cf65ca38abf7d8624c76955e1b4
jje951122 <76868571+jje951122@users.noreply.github.com>
2021-11-03 11:23:07 +0900

    유저 예약 페이지 접근시 이름, 번호 주소 반환

--- src/main/java/com/project/sroa_inqueryschedule_msa/service/CustomerInqueryService.java
+++ src/main/java/com/project/sroa_inqueryschedule_msa/service/CustomerInqueryService.java
@@ -2,10 +2,17 @@ package com.project.sroa_inqueryschedule_msa.service;
 
 import com.project.sroa_inqueryschedule_msa.dto.ResponseBrieflyCustomerScheduleInfo;
 import com.project.sroa_inqueryschedule_msa.dto.ResponseDetailCustomerScheduleInfo;
+import com.project.sroa_inqueryschedule_msa.model.UserInfo;
 
 import java.util.List;
 
 public interface CustomerInqueryService {
+
+
+
+
+    UserInfo findUserInfo(String id);
+
     List<String> findUserScheduleMonth(String id, String date);
 
     List<ResponseBrieflyCustomerScheduleInfo> findUserScheduleBriefly(String id, String date);

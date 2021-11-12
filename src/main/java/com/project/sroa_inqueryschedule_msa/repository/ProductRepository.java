0cf0b59cdd237bf4f0dedc4d5d8a15f24326315d
jje951122 <76868571+jje951122@users.noreply.github.com>
2021-11-12 17:45:16 +0900

    도커 파일 생성, AWS 배포 완료

--- src/main/java/com/project/sroa_inqueryschedule_msa/repository/ProductRepository.java
+++ src/main/java/com/project/sroa_inqueryschedule_msa/repository/ProductRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAll();
 
-    @Query(nativeQuery = true, value = "SELECT p.* FROM product p WHERE p.product_num = (SELECT s.product_num FROM Schedule s WHERE s.schedule_num=?1)")
+    @Query(nativeQuery = true, value = "SELECT p.* FROM product p WHERE p.product_num = (SELECT s.product_num FROM schedule s WHERE s.schedule_num=?1)")
     Product findByScheduleNum(Long scheduleNum);
 }
